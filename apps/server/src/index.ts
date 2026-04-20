969215712f6228d3817d29dd49d4c0e6fa118757
SelvaGaneshV <ganeshv@torus.tech>
2026-04-20 16:53:13 +0530

    removed some things

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,17 +1,13 @@
-import { createApi } from "@auth-practices/api";
 import { Hono } from "hono";
 
 import { env } from "@auth-practices/env/server";
 if (!env.SUPER_ADMIN_CORS_ORGIN) {
   throw new Error("Missing SUPER_ADMIN_CORS_ORGIN");
 }
-const api = createApi();
 
 const app = new Hono();
 
 app.get("/", (c) => c.text("Hello world"));
-
-app.route("/", api);
 
 export const includeFiles = "../../packages/**";
 
